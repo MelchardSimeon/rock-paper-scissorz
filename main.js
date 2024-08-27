69cef68f6cab8cf0f005d2d50755e9855227edff
MelchardSimeon <melchardsimeon71@gmail.com>
2024-08-27 17:07:19 +0800

    add function that randomly returns rock, paper , scissors

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+function getComputerChoice() {
+  let computerOptions = ["rock", "paper", "scissors"];
+  let randomNumbers = Math.floor(Math.random() * computerOptions.length);
+
+  if (randomNumbers === 0) {
+    console.log("rock");
+  } else if (randomNumbers === 1) {
+    console.log("paper");
+  } else if (randomNumbers === 2) {
+    console.log("scissors");
+  }
+}
+    
+getComputerChoice()
